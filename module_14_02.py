d461a231b711cd7dba598685f74fa762ed4b6f34
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-11-09 03:06:04 +0500

    Add files via upload

---
+++ module_14_02.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+connetion = sqlite3.connect('not_telegram.db')
+cursor = connetion.cursor()
+
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, email TEXT NOT NULL, "
+    "age INTEGER NOT NULL, balance INTEGER NOT NULL)")
+
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f'User{i}', f'example{i}@gmail.com', i * 10, 1000))
+
+cursor.execute(f"UPDATE Users SET balance = balance - 500 WHERE id % 2 != 0")
+cursor.execute("DELETE FROM Users WHERE id % 3 = 1")
+
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age !=60")
+users = cursor.fetchall()
+for i in users:
+    print(f"Имя: {i[0]} | Почта: {i[1]} | Возраст: {i[2]} | Баланс : {i[3]}")
+cursor.execute("DELETE FROM Users WHERE id = 6")
+cursor.execute("SELECT COUNT(id) FROM Users")
+count = cursor.fetchone()[0]
+print(count)
+cursor.execute("SELECT SUM(balance) FROM Users")
+total_balance = cursor.fetchone()[0]
+print(total_balance)
+cursor.execute("SELECT AVG(balance) FROM Users")
+average_balance = cursor.fetchone()[0]
+print(average_balance)
+cursor.connection.commit()
+cursor.connection.close()
