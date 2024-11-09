7c5fa01c3e3b59b65de54da8bb970c90a671a4e3
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-11-10 00:30:15 +0500

    Add files via upload

---
+++ module_14_03.py
@@ -0,0 +1,150 @@
+import logging
+
+import requests
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+api = ""
+
+formula_vesa = "10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5"
+get_buying_list_text = "Название: {} | Описание: описание {} | Цена: {}"
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+"""
+Новая клавиатура к заданию 'Доработка бота'
+"""
+
+product_inline_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Product1", callback_data="product_buying")],
+        [InlineKeyboardButton(text="Product2", callback_data="product_buying")],
+        [InlineKeyboardButton(text="Product3", callback_data="product_buying")],
+        [InlineKeyboardButton(text="Product4", callback_data="product_buying")]
+    ], resize_keyboard=True
+)
+
+"""
+Клавиатуры со старых заданий
+"""
+
+inline_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Рассчитать норму калорий", callback_data="calories")],
+        [InlineKeyboardButton(text="Формулы расчёта", callback_data="formulas")]
+    ], resize_keyboard=True
+)
+
+
+reply_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Рассчитать")],
+        [KeyboardButton(text="Информация")],
+        [KeyboardButton(text="Купить")]
+    ], resize_keyboard=True
+)
+
+
+def calculate_calories(age, growth, weight):
+    norma = 10 * weight + 6.25 * growth - 5 * age + 5
+    return norma
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью", reply_markup=reply_kb)
+
+
+"""
+Хэндлеры и функции по заданию 'Доработка бота' 
+"""
+
+
+@dp.message_handler(text="Купить")
+async def get_buying_list(message):
+    products = [
+        {'name': 'Product 1', 'description': '1', 'price': 100, 'url': "http://bit.ly/4flwaAo"},
+        {'name': 'Product 2', 'description': '2', 'price': 200, 'url': "https://bit.ly/3UIk1NP"},
+        {'name': 'Product 3', 'description': '3', 'price': 300, 'url': "https://bit.ly/3UIk6RD"},
+        {'name': 'Product 4', 'description': '4', 'price': 400, 'url': "https://bit.ly/3CezWgj"}
+    ]
+    for product in products:
+        await message.answer(get_buying_list_text.format(product['name'], product['description'], product['price']))
+        response = requests.get(product['url'], stream=True)
+        response.raise_for_status()
+        await message.answer_photo(photo=response.raw)
+    await message.answer('Выберите продукт для покупки:', reply_markup=product_inline_kb)
+
+
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+    await call.answer()
+
+
+"""
+Остальные хэндлеры и функции с прошлых заданий
+"""
+
+
+@dp.message_handler(text="Рассчитать")
+async def main_menu(message):
+    await message.answer('Выберите опцию:', reply_markup=inline_kb)
+
+
+@dp.callback_query_handler(text="formulas")
+async def get_formulas(call):
+    await call.message.answer(formula_vesa)
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите ваш возраст')
+    await UserState.age.set()
+    await call.answer()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите ваш рост')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите ваш вес')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = int(data.get('age'))
+    growth = int(data.get('growth'))
+    weight = int(data.get('weight'))
+    calories = calculate_calories(age, growth, weight)
+    await message.answer(f'Ваша норма калорий - {calories} ккал')
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_messages(message):
+    await message.answer('Введите команду /start, чтобы начать общение')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
